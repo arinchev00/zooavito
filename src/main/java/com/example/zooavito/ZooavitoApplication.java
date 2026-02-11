81ddef8d0ed87d0963a2a2302e7ba7b4dd5b3bee
tester <arinchev00@rambler.ru>
2026-02-11 16:01:13 +0300

    testCommit

---
+++ src/main/java/com/example/zooavito/ZooavitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooavito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooavitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooavitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zooavito/ZooavitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooavito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooavitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
